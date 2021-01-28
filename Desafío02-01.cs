926988b822997c966903f7833d87f979ab25e60e
Sofsil-28 <78062244+Sofsil-28@users.noreply.github.com>
2021-01-28 00:02:54 -0500

    Add files via upload

---
+++ Desafío02-01.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entradas 
+
+            // Variable y 
+            Console.Write("Ingrese el valor y: ");
+            double y = double.Parse(Console.ReadLine());
+
+
+            //Variable z 
+            Console.Write("Ingrese el valor z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            //proceso
+
+            //Encontrar  la hipotenusa "t"
+            double t = Math.Sqrt((y * y) + (z * z));
+            Console.WriteLine("La hipotenusa es igual a: " + t);
+
+            //Encontrar ángulo c
+            double c = Math.Asin(z / t);
+            double cGrad = c * 180.0/Math.PI ;
+            Console.WriteLine("El ángulo c es igual a:  " + cGrad);
+
+            //Encontrar el ángulo a
+
+            double a = 180 - (cGrad + 90);
+            Console.WriteLine("El ángulo a es igual:  " + a);
+
+
+        }
+    }
+}
