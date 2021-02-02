e6d8391e56ed89d0b4634730a5fa7fb0a6278ad1
Sofsil-28 <78062244+Sofsil-28@users.noreply.github.com>
2021-02-02 07:25:36 -0500

    Add files via upload

---
+++ S2_Desafio_01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    // tarifas parque arví 
+            //Entrada 
+
+            double salario = 908526;
+
+            // Entrada del usuario de la variable X (smmlv)
+            Console.Write("Ingrese su salario  X: ");
+            double x = double.Parse(Console.ReadLine());
+
+            if (x < salario * 2)
+            {
+                Console.WriteLine("Usted es tarifa A y debe pagar 2.500 pesos ");
+            }
+            else if ( salario * 2 <= x && x < salario * 4 ) 
+            {
+                Console.WriteLine("Usted es tarifa B y debe pagar 4.200 pesos ");
+            }
+            else if(salario * 4 <= x )
+            {
+                Console.WriteLine("Usted es tarifa c y debe pagar 10.800 pesos ");
+
+            }
+        }
+    }
+}
---
+++ S2_desafio_02.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2_t2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // entradas 
+            // salario mensual (s)
+
+            double s = 908526;
+
+            //salario diario (d)
+            double d = 30284.20;
+
+            //cuota
+            double cuota; 
+
+            // entrada de la variable z
+            Console.Write("Ingrese su salario  z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            if (z < s * 2)
+            {
+                cuota = d * 11.7 / 100;
+                Console.WriteLine("Usted es tarifa A ");
+                Console.WriteLine("Usted debe pagar " + cuota + " pesos de cuota moderadora  ");
+            }
+            else if (s * 2 <= z && z < s * 5)
+            {
+                cuota = d * 46.10 / 100;
+                Console.WriteLine("Usted es tarifa B ");
+                Console.WriteLine("Usted debe pagar " + cuota + " pesos de cuota moderadora  ");
+            }
+            else  if (s * 5 <= z)
+            {
+                cuota = d * 121.50 / 100;
+                Console.WriteLine("Usted es tarifa C ");
+                Console.WriteLine("Usted debe pagar " + cuota + " pesos de cuota moderadora  ");
+            }
+        }
+        
+    }
+}
