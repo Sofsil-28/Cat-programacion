b2a78d7240da57c7a52db9d08b21775ad62331e3
Sofsil-28 <78062244+Sofsil-28@users.noreply.github.com>
2021-02-23 03:14:50 -0500

    Add files via upload

---
+++ S5_T1Blackjack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace S5_t01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            Console.Write("¿Cuantos jugadores hay ?:  ");
+            double jugadores = double.Parse(Console.ReadLine());
+
+            jugadores = 6;
+            int contador = 0;
+            int maximo = 0;
+            int Ganador = 0;
+
+            if (jugadores >=2 && jugadores <= 5)
+            {
+                while (contador < jugadores)
+
+                {
+
+                    Console.WriteLine("\n\nBienvenido jugador " + (contador + 1));
+
+
+                    string respuesta = "s";
+                    int total = 0, carta1 = 0, carta2 = 0, carta3 = 0;
+                    carta1 = aleatorio.Next(1, 11);
+                    Console.WriteLine("Cartas:" + carta1);
+                    carta2 = aleatorio.Next(1, 11);
+                    Console.WriteLine("Cartas:" + carta2);
+
+                    total += carta1 + carta2;
+                    Console.WriteLine("Total:" + total);
+
+                    Console.Write("Desea parar   (s/n):  ");
+                    respuesta = Console.ReadLine();
+
+                    if (respuesta == "s")
+                    {
+                        Console.WriteLine("Blackjack");
+                    }
+                    else
+                    {
+                        Console.Write("Desea más cartas  (s/n):  ");
+                        respuesta = Console.ReadLine();
+                        while (respuesta == "s")
+                        {
+
+                            carta3 = aleatorio.Next(1, 11);
+
+                            Console.WriteLine("Cartas:" + carta3);
+
+                            total += carta3;
+                            Console.WriteLine("Total:" + total);
+
+                            if (total > 21)
+                            {
+                                Console.WriteLine("you lose ");
+                                total = 0;
+                                respuesta = "n";
+                            }
+                            else
+                            {
+                                Console.Write("Desea más cartas  (s/n):  ");
+                                respuesta = Console.ReadLine();
+                            }
+                            if (respuesta == "n")
+                            {
+                                Console.WriteLine("Blackjack");
+                            }
+                        }
+                    }
+
+                    if (total > maximo)
+                    {
+                        maximo = total;
+                        Ganador = contador;
+                    }
+                    contador++;
+
+                }
+
+                Console.WriteLine("Ganó el jugador: " + (Ganador + 1));
+
+            }
+            else
+            {
+                Console.WriteLine("No juega ");
+            }
+           
+
+        }
+    }
+}
---
+++ s5_Tuto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_Tut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicio
+            Random random = new Random();
+            int a = random.Next(0, 11);
+            Console.WriteLine("a:" + a);
+            int b = random.Next(0, 11);
+            Console.WriteLine("b:" + b);
+            int sumacorrecta = a + b;
+
+            Console.Write("¿cuanto es la suma de a + b ? :  ");
+
+            int respuesta = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            while (contador < 2 && respuesta != sumacorrecta)
+            {
+                Console.WriteLine("Validación incorrecta no puede continuar  :");
+                contador += 1;
+
+                a = random.Next(0, 11);
+                Console.WriteLine("a:" + a);
+                b = random.Next(0, 11);
+                Console.WriteLine("b:" + b);
+                sumacorrecta = a + b;
+
+                Console.Write("¿cuanto es la suma de a + b ? :  ");
+
+                respuesta = int.Parse(Console.ReadLine());
+
+
+
+            }
+
+            if (contador >= 2)
+            {
+                Console.WriteLine("perdiste :");
+            }
+
+            if (respuesta == sumacorrecta)
+            {
+                Console.WriteLine(" puede continuar  :");
+            }
+
+
+
+
+
+        }
+    }
+}
