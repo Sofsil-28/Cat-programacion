eee7ab965ec20370bb5365cc1782ccc1bf50b956
Sofsil-28 <78062244+Sofsil-28@users.noreply.github.com>
2021-03-11 08:15:52 -0500

    Add files via upload

---
+++ S8_Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace s8_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+            Console.WriteLine("-----ingrese una frase  :  ");
+            string frase = Console.ReadLine();
+            frase = frase.ToUpper();
+            string[] fraseU = { frase};
+           
+
+            Console.WriteLine("ingrese una palabra de la frase anteriormente ingresada   :  ");
+            string palabra = Console.ReadLine();
+            palabra = palabra.ToUpper();
+            string[] palabraU = { palabra };
+
+
+
+            string[] palaFrase = frase.Split(' ');
+
+            for (int i = 0; i < palaFrase.Length; i++)
+            {
+
+                if (palaFrase[i] == palabra)
+                {
+                    contador += 1;
+                    Console.WriteLine("encontrada la palabra ingresada en la pos:" + i  );
+                }
+               
+               
+
+
+            }
+            Console.WriteLine("el número de veces:" + contador);
+
+
+
+
+
+        }
+    }
+    
+}
